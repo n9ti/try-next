ec92fade8928fe3da5a1c6f6e329a2547a5539ac
Nati Namvong <nati@flyingcomma.com>
2017-06-03 13:48:45 +0700

    Updates

---
+++ _next/2c6fb0bb-ba8a-4863-8b88-a204d6f34c8d/page/post/index.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/post', function() {
+            var comp = module.exports=webpackJsonp([4],[],[213]);
+            return { page: comp.default }
+          })
+        
